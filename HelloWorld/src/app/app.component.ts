853036e76226e53e019ab6296ae13e4fe5e07f2c
omtiwari52 <tiwariom900@gmail.com>
2025-03-06 16:44:02 +0530

    [Om Tiwari] add: Ability to validate username at runtime feature added✨

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -13,7 +13,8 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com/";
-  username: String = '';
+  username: string = '';
+  nameError: string ='';
 
   ngOnInit():void{
     this.title = "Hello from BridgeLabz!!"
@@ -23,5 +24,15 @@ export class AppComponent {
   onClick($event:any){
     console.log("Save button is clicked", $event);
     window.open(this.url, "_blank");
+  }
+  
+  onInput($event:any){
+    console.log("Change event occurred!!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.username)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!!";
   }
 }
